86be14074c16235d19f01b40f4de9cd1ac8ff0ca
Roger Han <roger.hanhong@gmail.com>
2016-10-31 23:29:54 +1300

    Add a map view controller to show the full screen size map, also using UITapGestureRecognizer to detect user tap gesture

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var restaurantImageView:UIImageView!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var restaurantNameLabel: UILabel!
+    
+    @IBOutlet var mapView: MKMapView!
     
     var restaurant: Restaurant!
 
@@ -30,6 +33,43 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         tableView.estimatedRowHeight = 36.0
         tableView.rowHeight = UITableViewAutomaticDimension
         
+        
+        // UITapGestureRecognizer for detecting the tap gesture
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(showMap))
+        mapView.addGestureRecognizer(tapGestureRecognizer)
+        
+        // 
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: {
+        placemarks, error in
+            if error != nil {
+               // print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemarks
+                
+                let placemark = placemarks[0]
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                
+                if let location = placemark.location {
+                    // Display the annotation
+                    
+                    annotation.coordinate = location.coordinate
+                    self.mapView.addAnnotation(annotation)
+                    
+                    // Set the zoom level
+                    let region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 250, 250)
+                    self.mapView.setRegion(region, animated: false)
+                }
+            }
+        
+        })
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,6 +80,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
         return 4
+    }
+    
+    func showMap() {
+        performSegue(withIdentifier: "showMap", sender: self)
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -101,6 +145,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             let destinationController = segue.destination as! ReviewViewController
             destinationController.restaurant = restaurant
             
+        } else if segue.identifier == "showMap" {
+            let destinationController = segue.destination as! MapViewController
+            destinationController.restaurant = restaurant
         }
     }
 
