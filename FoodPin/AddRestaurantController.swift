6606daa53bdf7641870ca770c69ea97083eb6685
Roger Han <hong.han@fiserv.com>
2016-11-08 16:26:57 +1300

    Add the function to check if the textfield is null or not, and will not pass it if it's null

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -11,6 +11,31 @@ import UIKit
 class AddRestaurantController: UITableViewController, UINavigationControllerDelegate {
     
     @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var typeTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var yesButton: UIButton!
+    @IBOutlet var noButton: UIButton!
+    
+    
+    @IBAction func saveButton(_ sender: AnyObject) {
+        
+        if nameTextField.text == "" || locationTextField.text == "" || typeTextField.text == "" {
+             let alertController = UIAlertController(title: "Oops", message: "We can't proceed because one of the textfield is blank", preferredStyle: .alert)
+             let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+             alertController.addAction(alertAction)
+             present(alertController, animated: true, completion: nil)
+            
+            print("Name: \(nameTextField.text)")
+            print("Name: \(typeTextField.text)")
+            print("Name: \(locationTextField.text)")
+            print("Have you been here:")
+            
+//            dismiss(animated: true, completion: nil)
+//            performSegue(withIdentifier: "unwindToHomeScreen", sender: self)
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
