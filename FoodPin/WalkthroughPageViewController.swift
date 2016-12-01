fbea8dfbc8a4af11a647b6a0dc6de0a074aa80aa
Roger Han <hong.han@fiserv.com>
2016-12-01 14:30:34 +1300

    Replace the default page indicator with the custom one, and add a forward button at the bottom right cornor

--- FoodPin/WalkthroughPageViewController.swift
+++ FoodPin/WalkthroughPageViewController.swift
@@ -33,17 +33,17 @@ class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerD
         // Dispose of any resources that can be recreated.
     }
     
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return pageHeadings.count
-    }
-    
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        if let pageContentViewController = storyboard?.instantiateViewController(withIdentifier: "WalkthroughContentViewController") as? WalkthroughContentViewController {
-            
-            return pageContentViewController.index
-        }
-        return 0
-    }
+//    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+//        return pageHeadings.count
+//    }
+//    
+//    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        if let pageContentViewController = storyboard?.instantiateViewController(withIdentifier: "WalkthroughContentViewController") as? WalkthroughContentViewController {
+//            
+//            return pageContentViewController.index
+//        }
+//        return 0
+//    }
     
     // MARK: - UIPageViewControllerDataSource Methods
     
